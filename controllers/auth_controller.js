10396ced5512ad7e4eb759dbaed790dafe946637
ayman-its <mohamed.ayman@egypt-its.com>
2025-08-03 02:06:03 +0300

    adding the signup data

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,6 +1,7 @@
 const userService = require("../services/user_services");
 
 exports.signupStep1 = async (req, res) => {
+  console.log("Incoming body:", req.body);
   try {
     const user = await userService.createUserStep1(req.body);
     res.status(201).json({ success: true, userId: user._id });
@@ -11,7 +12,10 @@ exports.signupStep1 = async (req, res) => {
 
 exports.signupStep2 = async (req, res) => {
   try {
-    const user = await userService.completeUserStep2(req.params.userId, req.body);
+    const user = await userService.completeUserStep2(
+      req.params.userId,
+      req.body
+    );
     res.status(200).json({ success: true, message: "Profile completed", user });
   } catch (err) {
     res.status(400).json({ success: false, message: err.message });
